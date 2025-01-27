34f5a96f91362a0c271d4f189871e5f5841b5a62
Gabrielnfranca <weboneads01@gmail.com>
2025-01-27 16:17:38 -0300

    Fix: Atualiza configurações para resolver problemas de MIME type

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    port: 3000,
-    strictPort: true,
-    open: true,
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-  },
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    manifest: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          let extType = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          } else if (/woff|woff2/.test(extType)) {
-            extType = 'fonts';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
+        manualChunks: undefined
+      }
+    }
+  }
 });
